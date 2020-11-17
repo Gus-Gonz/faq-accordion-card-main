c1fca413e5b5a97a7824b512a20811f541c2413d
Gustavo Gonzalez <Gegf1234@Gmail.com>
2020-11-17 10:15:49 -0300

    minnor changes made with the Js group

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,31 +1,28 @@
-const liElement = document.querySelectorAll('li');
+const liElements = document.querySelectorAll('li');
 
 let currentElementDisplayed;
 
 function setDefaultStyle () {
     currentElementDisplayed.querySelector('h4').className = '';
-    currentElementDisplayed.querySelector('p').className = 'hiden';
+    currentElementDisplayed.querySelector('p').className = 'hidden';
     currentElementDisplayed.querySelector('span').className ='arrow-image';
 }
 
 function setDisplayedClass(target) {
     target.querySelector('h4').classList.toggle('bold-font');
-    target.querySelector('p').classList.toggle('hiden');
+    target.querySelector('p').classList.toggle('hidden');
     target.querySelector('span').classList.toggle('rotated');
 }
 
-
-liElement.forEach(element => {
+liElements.forEach(element => {
     element.addEventListener('click', (event) => {
         if (currentElementDisplayed == event.currentTarget){
             setDisplayedClass(event.currentTarget);
+        }else if (currentElementDisplayed){
+            setDefaultStyle();
+            setDisplayedClass(event.currentTarget);
         }else{
-            try {
-                setDefaultStyle()
-                setDisplayedClass(event.currentTarget)
-            }catch {
-                setDisplayedClass(event.currentTarget);
-            }
+            setDisplayedClass(event.currentTarget);
         }
         currentElementDisplayed = event.currentTarget;
     });
